724b0a04b91bda0e46b7442d7446d6256771227f
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-05-12 12:24:59 +0100

    Added Initial Storybook embedded link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
   <>
+    <main className={styles.wrapper}>
 <iframe
     src="https://664099e473cee1d4b7313e8f-nsiizhjekv.chromatic.com/?path=/story/example-button--primary"
-    width="100vw"
-    height="100vh"
+    width="100%"
+    height="100%"
 ></iframe>
+      </main>
   </>
   );
 }
