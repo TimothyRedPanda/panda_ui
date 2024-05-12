5f6082f3913b8f0a2019388bbd20dc2abfe91398
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-05-12 20:37:06 +0100

    Published version 0.1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.scss";
-const  buttonURL = "https://664099e473cee1d4b7313e8f-apxgtgpjug.chromatic.com/?path=/docs/pandaui-button--docs"
+const  buttonURL = "https://664099e473cee1d4b7313e8f-dwvpbtpoxo.chromatic.com/?path=/docs/design-system"
 
 export default function Home() {
   return (
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 import {Button} from './stories/button/Button'
+import {Input} from './stories/input/Input'
+
 export {Button};
+
+export {Input};
