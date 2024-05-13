7c6405a325cb8c154c75deb7643d8d08e72a0d46
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-05-13 20:47:40 +0100

    Published version 0.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Button} from './stories/button/Button'
 import {Input} from './stories/input/Input'
+import {Label} from './stories/label/label'
 
 export {Button};
-
+export {Label};
 export {Input};
